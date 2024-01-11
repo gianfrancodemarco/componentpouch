ac11b6cbb4658c8a63b2488d839a4bd88250de52
gianfrancodemarco <gianfrancodemarco@users.noreply.github.com>
2024-01-11 19:16:15 +0000

    autopep8 action fixes

--- componentpouch/tools.py
+++ componentpouch/tools.py
@@ -5,30 +5,36 @@ import typer
 
 app = typer.Typer()
 
+
 def install_vscode():
     typer.echo("🚀 Installing VSCode...")
     subprocess.run(["sudo", "apt-get", "install", "code"])
     typer.echo("✅ VSCode installation completed.")
+
 
 def install_tmux():
     typer.echo("🚀 Installing tmux...")
     subprocess.run(["sudo", "apt-get", "install", "tmux"])
     typer.echo("✅ tmux installation completed.")
 
+
 def install_k9s():
     typer.echo("🚀 Installing k9s...")
     subprocess.run(["kubectl", "krew", "install", "k9s"])
     typer.echo("✅ k9s installation completed.")
+
 
 def install_docker():
     typer.echo("🚀 Installing Docker...")
     subprocess.run(["sudo", "apt-get", "install", "docker-ce"])
     typer.echo("✅ Docker installation completed.")
 
+
 def install_kubectl():
     typer.echo("🚀 Installing kubectl...")
     subprocess.run(["sudo", "apt-get", "install", "kubectl"])
     typer.echo("✅ kubectl installation completed.")
+
 
 allowed_tools = {
     'vscode': install_vscode,
@@ -38,19 +44,24 @@ allowed_tools = {
     'kubectl': install_kubectl
 }
 
+
 @app.command(name='list')
 def _list():
     for tool in allowed_tools:
         typer.echo(tool)
 
+
 @app.command()
 def install(
-    tools: List[str] = typer.Argument(..., help="List of tools to install (used with 'install' command)", autocompletion=lambda: list(allowed_tools.keys())),
+    tools: List[str] = typer.Argument(...,
+                                      help="List of tools to install (used with 'install' command)",
+                                      autocompletion=lambda: list(allowed_tools.keys())),
 ):
     """Install required tools"""
     for tool in tools:
         if tool.lower() not in allowed_tools:
-            typer.echo(f"Error: {tool} is not a valid tool. Valid tools are: {', '.join(allowed_tools)}")
+            typer.echo(
+                f"Error: {tool} is not a valid tool. Valid tools are: {', '.join(allowed_tools)}")
             raise typer.Abort()
 
         typer.echo(f"Installing {tool}...")
